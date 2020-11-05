c6b989dca51acd7fcf2ca471e1f10ebdfd98507f
NURDIN NURMANBETOV <NURDIN.NURMANBETOV@GMAIL.COM>
2020-11-05 00:38:59 -0500

    Basic calculator functionality is done except for /, needs code optimization, shortening

---
+++ calculator.js
@@ -0,0 +1,186 @@
+$(document).ready(function(){
+    console.log("Hello world");
+
+    var on = false;
+    var num1 = '';
+    var num2 = '';
+    var temp = '';
+    var operand = '';
+    
+    
+// need to add event listener to button div and single funtion extract button's value and push to screen
+    $("#on").click(function(){
+        if (!on){
+        $("#screen").html('');
+        $("#screen").append(0)
+        on = true;
+        }
+    })
+
+    $("#off").click(function(){
+        on = false;
+        $("#screen").html('');
+        temp = '';
+    })
+    
+    $("#7").click(function(){
+        if (on && temp.length < 10) {
+        temp += 7;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#8").click(function(){
+        if (on && temp.length < 10) {
+        temp += 8;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#9").click(function(){
+        if (on && temp.length < 10) {
+        temp += 9;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#4").click(function(){
+        if (on && temp.length < 10) {
+        temp += 4;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#5").click(function(){
+        if (on && temp.length < 10) {
+        temp += 5;
+        $("#screen").html(temp);
+        }
+    });
+   
+    $("#6").click(function(){
+        if (on && temp.length < 10) {
+        temp += 6;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#1").click(function(){
+        if (on && temp.length < 10) {
+        temp += 1;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#2").click(function(){
+        if (on && temp.length < 10) {
+        temp += 2;
+        $("#screen").html(temp);
+        }
+    });
+   
+    $("#3").click(function(){
+        if (on && temp.length < 10) {
+        temp += 3;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#0").click(function(){
+        if (on && temp.length < 10 && temp !== '0') {
+        temp += 0;
+        $("#screen").html(temp);
+        }
+    });
+
+    $("#c").click(function(){
+        if (on) {
+        temp='';
+        num1 = 0;
+        num2 = 0;
+        $("#screen").html('');
+        $("#screen").append(0)
+        }
+    });
+
+    $("#plus").click(function(){
+        if (on) {
+        num1 = Number(temp);
+        temp = '';
+        operand = '+';
+        $("#screen").html('');
+        $("#screen").append(0)
+        }
+    });
+    $("#minus").click(function(){
+        if (on) {
+        num1 = Number(temp);
+        temp = '';
+        operand = '-';
+        $("#screen").html('');
+        $("#screen").append(0)
+        }
+    });
+    $("#product").click(function(){
+        if (on) {
+        num1 = Number(temp);
+        temp = '';
+        operand = '*';
+        $("#screen").html('');
+        $("#screen").append(0)
+        }
+    });
+
+    $("#equals").click(function(){
+        if (on && operand === '+') {
+            num2 = Number(temp);
+            temp = '';
+            var result;
+            result = (num1 + num2);
+            $("#screen").html('');
+            if (result < 10000000000) {
+            
+            $("#screen").append(result)
+            num1 = 0;
+            num2 = 0;
+            }
+            else {
+                $("#screen").append("Out of range")
+            }
+        }
+
+        if (on && operand === '-') {
+            num2 = Number(temp);
+            temp = '';
+            var result;
+            result = (num1 - num2);
+            $("#screen").html('');
+            if (result < 10000000000) {
+            
+            $("#screen").append(result)
+            num1 = 0;
+            num2 = 0;
+            }
+            else {
+                $("#screen").append("Out of range")
+            }
+        }
+
+        if (on && operand === '*') {
+            num2 = Number(temp);
+            temp = '';
+            var result;
+            result = (num1 * num2);
+            $("#screen").html('');
+            if (result < 10000000000) {
+            
+            $("#screen").append(result)
+            num1 = 0;
+            num2 = 0;
+            }
+            else {
+                $("#screen").append("Out of range")
+            }
+        }
+    });
+});
